9063bccdc7f4eafbfd32d2a00ce69e79a7c806cd
Badreddin Nakhil <badreddinnakhil@MacBook-Pro-di-Badreddin.local>
2026-03-02 00:44:24 +0100

    Add favicon using logo.png

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es" className="scroll-smooth">
       <head>
+        <link rel="icon" href="/logo.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
