61d723d25edd8e515631866f6794684709977b34
Badreddin Nakhil <badreddinnakhil@MacBook-Pro-di-Badreddin.local>
2026-05-09 04:46:45 +0200

    feat: añadir el ID real del meta pixel

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -3,22 +3,9 @@ globalThis.__BUILD_MANIFEST = {
     "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [
-    "static/chunks/1eb3f_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_7588ea36._.js",
-    "static/chunks/1eb3f_next_dist_compiled_react-dom_c83d7959._.js",
-    "static/chunks/1eb3f_next_dist_compiled_react-server-dom-turbopack_3c43a22f._.js",
-    "static/chunks/1eb3f_next_dist_compiled_next-devtools_index_959d1210.js",
-    "static/chunks/1eb3f_next_dist_compiled_109e256b._.js",
-    "static/chunks/1eb3f_next_dist_client_fb220fdc._.js",
-    "static/chunks/1eb3f_next_dist_7c86ccc4._.js",
-    "static/chunks/1eb3f_@swc_helpers_cjs_8137fb43._.js",
-    "static/chunks/Documents_crm ibericos gourmet_a0ff3932._.js",
-    "static/chunks/turbopack-Documents_crm ibericos gourmet_652f853e._.js"
-  ]
+  "rootMainFiles": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
